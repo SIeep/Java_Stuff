445fdd714e2bf6bbff1a4b7f04c3622be4010baf
SIeep <brandon_n_evans@live.com>
2019-05-13 17:45:12 -0500

    practice compiling

--- InputOutput/Greeter.java
+++ InputOutput/Greeter.java
@@ -5,13 +5,13 @@ public class Greeter {
   public static void main(String[] args) {
     Console console = System.console();
     // TODO:  1. Create a new String named place and assign it a value of your choosing.
-    String place = "Chicago";
+    String place = "Java";
     // TODO:  2. Using the provided console object, 
     //           prompt the user "What is your first name?" and store that in a variable
     String firstName = console.readLine("What's your first name? ");
     // TODO:  3. Print out to the console "Hello <FIRST_NAME>!  Welcome to <PLACE>."
-    console.printf("Hello %s! Welcome to %s.", firstName, place);
-    console.printf("Thanks for testing me!");
+    console.printf("Hello %s! Welcome to %s.\n", firstName, place);
+    console.printf("Thanks for testing me!\n");
   }
   
 
