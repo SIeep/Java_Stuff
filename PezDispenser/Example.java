370286530c5e97119312aed861d05a2b257f2215
Brandon Evans <bnevans@fb.com>
2019-05-14 12:51:02 -0500

    add multiple java files in repo and create PezDispenser object

--- PezDispenser/Example.java
+++ PezDispenser/Example.java
@@ -1,7 +1,10 @@
 public class Example {
 
 	public static void main(String[] args) {
+		PezDispenser dispenser = new PezDispenser();
 		System.out.println("I'm creating a Pez dispenser.");
+		System.out.printf("The current dispenser is a %s dispenser %n",
+			dispenser.characterName);
 	}
 
 }
