6c51e56cd32659975301b6aa21f5d8d974d19a68
Brandon Evans <bnevans@fb.com>
2019-05-15 10:08:17 -0500

    add logic to Prompter.java

--- Hangman/Hangman.java
+++ Hangman/Hangman.java
@@ -3,5 +3,12 @@ public class Hangman {
   public static void main(String[] args) {
     // Your incredible code goes here...
     Game game = new Game("brandon");
+    Prompter prompter = new Prompter(game);
+    boolean isHit = prompter.promptForGuess();
+    if (isHit) {
+    	System.out.println("You got a hit!");
+    } else {
+    	System.out.println("Missed. Didn't hit.");
+    }
   }
 }
