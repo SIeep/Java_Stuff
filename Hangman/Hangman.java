89285fc386a989659e3e1b969537f579ae556aaf
Brandon Evans <bnevans@fb.com>
2019-05-15 10:54:51 -0500

    add getCurrentProgress and displayCurrentProgress methods

--- Hangman/Hangman.java
+++ Hangman/Hangman.java
@@ -4,11 +4,13 @@ public class Hangman {
     // Your incredible code goes here...
     Game game = new Game("brandon");
     Prompter prompter = new Prompter(game);
+    prompter.displayProgress();
     boolean isHit = prompter.promptForGuess();
     if (isHit) {
     	System.out.println("You got a hit!");
     } else {
     	System.out.println("Missed. Didn't hit.");
     }
+    prompter.displayProgress();
   }
 }
