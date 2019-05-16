1c9b04f89dbcb479ce5dfdf78dd7ef4e04227bc0
Brandon Evans <bnevans@fb.com>
2019-05-16 09:18:42 -0500

    add enhanced for loop on array

--- Loop_Practice/Explore.java
+++ Loop_Practice/Explore.java
@@ -2,7 +2,11 @@ public class Explore {
  
   public static void main(String[] args) {
     String[] friends = {"Brandon", "Kayla", "Smith"};
-    System.out.println(friends[0]);
+    // System.out.println(friends[0]);
+
+    for (String friend : friends) {
+    	System.out.println("Hi there, " + friend + "! How are you?\n");
+    }
   }
   
 }
