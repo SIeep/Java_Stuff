250e8fc8a2c424cc386e141d332777b804196b31
Brandon Evans <bnevans@fb.com>
2019-05-16 09:31:06 -0500

    add regular for loop for practice

--- Loop_Practice/Explore.java
+++ Loop_Practice/Explore.java
@@ -5,7 +5,13 @@ public class Explore {
     // System.out.println(friends[0]);
 
     for (String friend : friends) {
-    	System.out.println("Hi there, " + friend + "! How are you?\n");
+    	System.out.println("Hi there, " + friend + "! I'm testing an enhanced for loop.\n");
+    }
+
+  	System.out.println("----------------------");  
+
+    for (int i = 0; i < friends.length; i++) {
+    	System.out.printf("Hi %s, I'm testing a regular for loop.%n", friends[i]);
     }
   }
   
