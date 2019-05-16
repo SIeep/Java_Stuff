deb034f16234fd08f805f2f7a5fefad142e298e8
Brandon Evans <bnevans@fb.com>
2019-05-16 10:26:17 -0500

    add more advanced for loop for practice

--- Loop_Practice/Explore.java
+++ Loop_Practice/Explore.java
@@ -3,15 +3,20 @@ public class Explore {
   public static void main(String[] args) {
     String[] friends = {"Brandon", "Kayla", "Smith"};
     // System.out.println(friends[0]);
-
     for (String friend : friends) {
     	System.out.println("Hi there, " + friend + "! I'm testing an enhanced for loop.\n");
-    }
-
+    };
   	System.out.println("----------------------");  
-
     for (int i = 0; i < friends.length; i++) {
     	System.out.printf("Hi %s, I'm testing a regular for loop.%n", friends[i]);
+    };
+    System.out.println("----------------------");  
+    int[] brandonScoreCard = {1, 4, 8, 1, 3, 2, 7, 3, 6, 2, 5, 5, 1};
+    for (int i = 0; i < brandonScoreCard.length; i++) {
+    	System.out.printf(" The score for hole %d is %d. %n",
+    	i + 1, 
+    	brandonScoreCard[i], 
+    	brandonScoreCard[i]);
     }
   }
   
