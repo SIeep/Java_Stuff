8892bd833128fb908fccfd38771ce0802f50e858
Brandon Evans <bnevans@fb.com>
2019-05-16 13:14:47 -0500

    add for loops and stuff

--- Cow/Arrays.java
+++ Cow/Arrays.java
@@ -1,18 +1,29 @@
 public class Arrays {
   
   public static void main(String[] args) {
-    Console console = System.console();
+  	int j = 0;
     
     // TODO: 1. Create an array of good cow names (this should be a String array).
-    
+    String[] cowNames = {
+    	"Betty",
+    	"Frank",
+    	"Lassy",
+    	"Sharon",
+    	"Dog"
+    };
     
     // TODO: 2. Create an array of Cows the same length as your 'names' array.
-    
+   	Cow[] cows = new Cow[cowNames.length];
     
     // TODO: 3. Use a for loop to populate the 'cows' array with Cows using names from the 'names' array.
-    
-    
+    for (int i = 0; i < cowNames.length; i++) {
+    	cows[i] = new Cow(cowNames[i]);
+    }
     // TODO: 4. Use an enhanced for loop to print the name of each Cow.
-    
-  }
+   
+   	for (Cow cow : cows) {
+   		System.out.printf("Cow name: %s %n", cow.getName());
+   	} 
+
+    }
 }
