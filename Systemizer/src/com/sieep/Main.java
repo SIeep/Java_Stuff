dd297f563d497d54fba71865fbe1410e5711f70f
Brandon Evans <bnevans@fb.com>
2019-05-17 13:14:51 -0500

    add Systemizer IDE practice

---
+++ Systemizer/src/com/sieep/Main.java
@@ -0,0 +1,13 @@
+package com.sieep;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("-------------");
+        System.out.println("Hello world!");
+        System.out.println("Testing from IDE!");
+        System.out.println("This is my first time ever using an IDE! Weird.");
+        System.out.println("-------------");
+    }
+}
