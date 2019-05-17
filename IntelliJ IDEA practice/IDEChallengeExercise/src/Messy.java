16281272af8cd826c442f53c9eda7ff1964dffc1
Brandon Evans <bnevans@fb.com>
2019-05-17 15:03:59 -0500

    practice keyboard shortcuts and rearrange lines

--- IntelliJ IDEA practice/IDEChallengeExercise/src/Messy.java
+++ IntelliJ IDEA practice/IDEChallengeExercise/src/Messy.java
@@ -3,13 +3,14 @@ import java.util.List;
 
 public class Messy {
     public static void main(String[] args) {
-        System.out.println("five");
         System.out.println("one");
-        System.out.println("six");
-        System.out.println("four");
         System.out.println("two");
-        Please comment out this line and
-        this line as well with a hotkey that does multi - line commenting
+        System.out.println("three");
+        System.out.println("four");
+        System.out.println("five");
+        System.out.println("six");
+//        Please comment out this line and
+//        this line as well with a hotkey that does multi - line commenting
         List<String> numberWords = Arrays.asList("six", "seven", "eight", "nine");
         for (String numberWord : numberWords) {
             // Use the sout shortcut to write out numberWord;
