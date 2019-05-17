2b786106794c64db60a2e196d6581be59e0b5662
Brandon Evans <bnevans@fb.com>
2019-05-17 15:00:29 -0500

    Add Messy.java

---
+++ IntelliJ IDEA practice/IDEChallengeExercise/src/Messy.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Messy {
+    public   static void main(String[]      args){
+System.out.println("five");System.out.println("one");
+    System.out.println("six");
+    System.out.println( "four"       );     System.out.println("two");
+    Please comment out this line and
+    this line as well with a hotkey that does multi-line commenting
+    List<String> numberWords = Arrays.asList("six", "seven", "eight", "nine");
+    for (String numberWord:         numberWords) {
+      // Use the sout shortcut to write out numberWord;
+      }
+    }}
