3456456c6d7adbe251b59e209b49b8373035c4e0
SIeep <brandon_n_evans@live.com>
2019-05-17 18:01:54 -0500

    add some vars and a enhanced for loop without looking at refs

--- RandomTestApp/src/com/company/Main.java
+++ RandomTestApp/src/com/company/Main.java
@@ -1,10 +1,24 @@
 package com.company;
 
+import javax.sound.midi.Soundbank;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String myName = "brandon";
+        int i = -0;
+        String myName = "Brandon";
         System.out.println(myName);
+        String[] nicknames = {
+                "Thug Nasty",
+                "Slim Shady",
+                "Lil Bambi",
+                "B-Rad"
+        };
+
+        for (String name : nicknames) {
+            System.out.printf("Hi, my name is %s but you can call me %s if you'd like!%n", myName, nicknames[i]);
+            i++;
+        }
     }
 }
