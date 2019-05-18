44514569d4d63977a963ef12a1dc628097945b63
Brandon Evans <bnevans@fb.com>
2019-05-18 09:07:16 -0500

    add mArtist,mTitle,mVideoURL + equals() and hashCode()

--- IntelliJ IDEA practice/Karaoke/src/com/teamtreehouse/model/Song.java
+++ IntelliJ IDEA practice/Karaoke/src/com/teamtreehouse/model/Song.java
@@ -1,9 +1,26 @@
 package com.teamtreehouse.model;
 
+import java.util.Objects;
+
 public class Song {
     protected String mArtist;
     protected String mTitle;
     protected String mVideoUrl;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Song song = (Song) o;
+        return Objects.equals(mArtist, song.mArtist) &&
+                Objects.equals(mTitle, song.mTitle) &&
+                Objects.equals(mVideoUrl, song.mVideoUrl);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mArtist, mTitle, mVideoUrl);
+    }
 
     public Song(String artist, String title, String videoUrl) {
         mArtist = artist;
--- IntelliJ IDEA practice/Karaoke/src/com/teamtreehouse/model/SongRequest.java
+++ IntelliJ IDEA practice/Karaoke/src/com/teamtreehouse/model/SongRequest.java
@@ -1,4 +1,51 @@
 package com.teamtreehouse.model;
 
+import java.util.Objects;
+
 public class SongRequest {
+    private Song mSong;
+    private String mSingerName;
+
+    public SongRequest(String singerName, Song song) {
+        mSingerName = singerName;
+        mSong = song;
+    }
+
+    public Song getSong() {
+        return mSong;
+    }
+
+    public void setSong(Song song) {
+        mSong = song;
+    }
+
+    public String getSingerName() {
+        return mSingerName;
+    }
+
+    public void setSingerName(String singerName) {
+        mSingerName = singerName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SongRequest that = (SongRequest) o;
+        return mSong.equals(that.mSong) &&
+                mSingerName.equals(that.mSingerName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mSong, mSingerName);
+    }
+
+    @Override
+    public String toString() {
+        return "SongRequest{" +
+                "mSong=" + mSong +
+                ", mSingerName='" + mSingerName + '\'' +
+                '}';
+    }
 }
