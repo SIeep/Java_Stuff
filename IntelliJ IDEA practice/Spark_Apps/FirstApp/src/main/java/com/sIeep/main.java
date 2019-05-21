93aaeab69bd43f2178ec885192c57c8a53bae8a8
Brandon Evans <bnevans@fb.com>
2019-05-21 12:59:23 -0500

    add Spark routes for hello world

--- IntelliJ IDEA practice/Spark_Apps/FirstApp/src/main/java/com/sIeep/main.java
+++ IntelliJ IDEA practice/Spark_Apps/FirstApp/src/main/java/com/sIeep/main.java
@@ -1,4 +1,10 @@
 package com.sIeep;
 
+import static spark.Spark.*;
+
 public class main {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+        get("/", (request, response) -> "Hello students!" );
+    }
 }
