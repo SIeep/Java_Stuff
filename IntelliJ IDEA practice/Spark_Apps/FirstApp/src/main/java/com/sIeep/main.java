bcc1168e9a2372a43a1f9db9702e46874685f1ea
Brandon Evans <bnevans@fb.com>
2019-05-21 15:08:22 -0500

    add a lot of stuff and create new Vue/Java test app

--- IntelliJ IDEA practice/Spark_Apps/FirstApp/src/main/java/com/sIeep/main.java
+++ IntelliJ IDEA practice/Spark_Apps/FirstApp/src/main/java/com/sIeep/main.java
@@ -6,6 +6,7 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class main {
+
     public static void main(String[] args) {
         get("/", (request, response) -> {
             return new ModelAndView(null, "index.hbs");
