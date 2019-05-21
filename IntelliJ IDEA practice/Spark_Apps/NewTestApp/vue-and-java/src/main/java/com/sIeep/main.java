2c3f20763415f95627a4f1c34a8af24781d4605d
Brandon Evans <bnevans@fb.com>
2019-05-21 15:31:38 -0500

    testing more gradle

---
+++ IntelliJ IDEA practice/Spark_Apps/NewTestApp/vue-and-java/src/main/java/com/sIeep/main.java
@@ -0,0 +1,9 @@
+package com.sIeep;
+
+import static spark.Spark.*;
+
+public class main {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World!");
+    }
+}
