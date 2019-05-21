0d1d29aeebdc810c845059565f12dd692145b342
Brandon Evans <bnevans@fb.com>
2019-05-21 13:49:18 -0500

    add handlebars template engine

--- IntelliJ IDEA practice/Spark_Apps/FirstApp/src/main/java/com/sIeep/main.java
+++ IntelliJ IDEA practice/Spark_Apps/FirstApp/src/main/java/com/sIeep/main.java
@@ -1,10 +1,14 @@
 package com.sIeep;
 
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 import static spark.Spark.*;
 
 public class main {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World");
-        get("/", (request, response) -> "Hello students!" );
+        get("/", (request, response) -> {
+            "Hello students!"
+        }, new HandlebarsTemplateEngine());
     }
 }
