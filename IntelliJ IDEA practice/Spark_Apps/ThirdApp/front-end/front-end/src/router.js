c97ce38de8253e9e33799f954fcf35fd30566345
Brandon Evans <bnevans@fb.com>
2019-05-22 13:42:39 -0500

    add sign in Vue component

--- IntelliJ IDEA practice/Spark_Apps/ThirdApp/front-end/front-end/src/router.js
+++ IntelliJ IDEA practice/Spark_Apps/ThirdApp/front-end/front-end/src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
 import Test from "./views/Test.vue";
+import Signin from "./views/Sign-in.vue";
 
 Vue.use(Router);
 
@@ -18,6 +19,11 @@ export default new Router({
       path: "/test",
       name: "test",
       component: Test
+    },
+    {
+      path: "/sign-in",
+      name: "signin",
+      component: Signin
     },
     {
       path: "/about",
---
+++ IntelliJ IDEA practice/Spark_Apps/ThirdApp/front-end/front-end/src/views/Sign-in.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="sign-in">
+        <h1>This is the sign-in page</h1>
+    </div>
+</template>
+
+<script>
+    import axios from "axios";
+
+    export default {
+        data: function() {
+            return {
+                message: "Welcome to the test page!",
+                myName: ""
+            };
+        },
+
+        created: function() {
+            axios.get("/brandon").then(response => {
+                this.myName = response.data;
+            });
+        },
+        // methods: {
+        //     // createPost: function() {
+        //     //     var params = {
+        //     //         text: this.createText
+        //     //     };
+        //     //     console.log("creating post...")
+        //     //     axios.post('/api/posts', params).then(
+        //     //         response => {
+        //     //             console.log(response);
+        //     //         });
+        //     //     location.reload();
+        //     },
+        // }
+    }
+</script>
+
+<style scoped>
+
+</style>
+
+<style scoped>
+
+</style>
