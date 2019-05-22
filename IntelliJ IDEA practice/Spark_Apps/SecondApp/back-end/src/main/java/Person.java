bd09ef2a1e0f65052548498c82b30171bfe8f513
SIeep <brandon_n_evans@live.com>
2019-05-21 19:37:56 -0500

    fetch my name using Spark

---
+++ IntelliJ IDEA practice/Spark_Apps/SecondApp/back-end/src/main/java/Person.java
@@ -0,0 +1,8 @@
+public class Person {
+        static String name = "Brandon";
+
+        public static void main(String[] args) {
+            Person brandon = new Person();
+            System.out.println(brandon.name);
+        }
+}
