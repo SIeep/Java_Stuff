4f99bbe34802d00eed4f1373fd95270124109506
Brandon Evans <bnevans@fb.com>
2019-05-22 08:37:43 -0500

    create person object from People class

---
+++ IntelliJ IDEA practice/Spark_Apps/ThirdApp/thirdapp-back-end/src/main/java/People.java
@@ -0,0 +1,8 @@
+public class People {
+    String name = "brandon";
+
+    public static void main(String[] args) {
+        People person = new People();
+        System.out.println(person.name);
+    }
+}
