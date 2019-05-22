49fdddc209ea25b699b47ab7300f97f0530a5d70
SIeep <brandon_n_evans@live.com>
2019-05-21 19:28:16 -0500

    add Gradle and Spark to SecondApp test

--- IntelliJ IDEA practice/Spark_Apps/SecondApp/backend/src/views/Home.vue
+++ IntelliJ IDEA practice/Spark_Apps/SecondApp/backend/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your poopy App"/>
   </div>
 </template>
 
