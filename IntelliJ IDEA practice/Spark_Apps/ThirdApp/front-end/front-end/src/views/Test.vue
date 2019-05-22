94a77693d2e9bfc53c1b4fe0c54656b3f65d47ef
Brandon Evans <bnevans@fb.com>
2019-05-22 08:55:30 -0500

    add test text

--- IntelliJ IDEA practice/Spark_Apps/ThirdApp/front-end/front-end/src/views/Test.vue
+++ IntelliJ IDEA practice/Spark_Apps/ThirdApp/front-end/front-end/src/views/Test.vue
@@ -1,12 +1,47 @@
 <template>
     <div class="test">
         <h1>This is a test page</h1>
+        <h1>Text</h1>
+        <h1>Message: {{message}}</h1>
+        <h1>Name: {{myName}}</h1>
     </div>
 </template>
 
+<!--<script>-->
+<!--    export default {-->
+<!--        name: "Test"-->
+<!--    }-->
+<!--</script>-->
+
 <script>
+    import axios from "axios";
+
     export default {
-        name: "Test"
+        data: function() {
+            return {
+                message: "Welcome to Avert.relapse!",
+                myName: ""
+            };
+        },
+
+        created: function() {
+            axios.get("/hello").then(response => {
+                this.myName = response.data;
+            });
+        },
+        // methods: {
+        //     // createPost: function() {
+        //     //     var params = {
+        //     //         text: this.createText
+        //     //     };
+        //     //     console.log("creating post...")
+        //     //     axios.post('/api/posts', params).then(
+        //     //         response => {
+        //     //             console.log(response);
+        //     //         });
+        //     //     location.reload();
+        //     },
+        // }
     }
 </script>
 
