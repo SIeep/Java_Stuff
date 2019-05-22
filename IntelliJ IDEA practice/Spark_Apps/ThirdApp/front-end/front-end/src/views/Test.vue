5d0a9282f35a5e89ce98b8dbb228f4d3ecc7174b
Brandon Evans <bnevans@fb.com>
2019-05-22 08:47:21 -0500

    add test page to front end

---
+++ IntelliJ IDEA practice/Spark_Apps/ThirdApp/front-end/front-end/src/views/Test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="test">
+        <h1>This is a test page</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Test"
+    }
+</script>
+
+<style scoped>
+
+</style>
